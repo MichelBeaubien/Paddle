d686451f7e43bfad8075eb02fe8b35ef6693c5e0
Michel Beaubien <mBeaubien@vicimus.com>
2019-01-21 14:54:45 -0500

    Stephanes website begining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ShowcaseComponent} from './showcase/showcase.component';
+import {ContactComponent} from './contact/contact.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'showcase', component: ShowcaseComponent},
+    {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ShowcaseComponent,
+        ContactComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
