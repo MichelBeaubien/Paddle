bc6435804ec64d545b51883ec975da82eabf5f94
Michel Beaubien <mBeaubien@vicimus.com>
2019-01-22 22:17:36 -0500

    showcase stuff

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-showcase',
-  templateUrl: './showcase.component.html',
-  styleUrls: ['./showcase.component.scss']
+    selector: 'app-showcase',
+    templateUrl: './showcase.component.html',
+    styleUrls: ['./showcase.component.scss']
 })
 export class ShowcaseComponent implements OnInit {
+    /**
+     * Showcase Category
+     */
+    public showCase = 'Fish';
 
-  constructor() { }
+    /**
+     * Showcases array
+     */
+    public showcases = [
+        'Fish',
+        'Cartoon',
+        'Hockey',
+        'Antique',
+        'Locations',
+        'Wilderness',
+        'Canada',
+        'Pride'
+    ];
 
-  ngOnInit() {
-  }
+    /**
+     * Showcase All
+     */
+    public showEverything = false;
 
+    constructor() {}
+
+    public ngOnInit() {}
+
+    public showcase(showing: string): void {
+        if (!showing) {
+            return;
+        }
+        this.showCase = showing;
+        this.showEverything = false;
+        if (showing === 'all') {
+            this.showEverything = true;
+            return;
+        }
+    }
 }
