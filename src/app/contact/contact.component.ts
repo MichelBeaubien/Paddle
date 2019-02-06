9c3b4fc9027e770a8f36981541337b64aaa2d33a
Michel Beaubien <mBeaubien@vicimus.com>
2019-02-05 21:57:48 -0500

    phone number clean up

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,14 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+    /**
+     * Cleaned phone number - EX: (123)-123-1234
+     */
+    public cleanedNumber: any = '';
+
+    /**
+     * Array where the form information goes.
+     */
     public model: any = {};
 
     constructor() {
@@ -14,7 +22,39 @@ export class ContactComponent implements OnInit {
     ngOnInit() {
     }
 
-    onSubmit() {
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+    /**
+     * When Form is submitted
+     *
+     * @returns {void}
+     */
+    public onSubmit(): void {
+        // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+        this.phoneNumber(this.model.phone);
+    }
+
+    /**
+     * Return the phone number to look like: (123)-123-1234
+     *
+     * @param {string} phoneNumber
+     */
+    public phoneNumber(phoneNumber: string): void {
+        const cleaned = phoneNumber.includes('(') || phoneNumber.includes(')') || phoneNumber.includes('-');
+
+        this.cleanedNumber = cleaned;
+        // const openBracket = phoneNumber.includes('(');
+        // const closeBracket = phoneNumber.includes(')');
+        //
+        // const removeBrackets = phoneNumber.replace('(', '').replace(')', '');
+        // const areaCode = phoneNumber.substr(0, 3)
+        // const prefix = phoneNumber.substr(3, 3);
+        // const number = phoneNumber.substr(6, 8);
+        //
+        // if (openBracket || closeBracket) {
+        //     phoneNumber.replace('(', '').replace(')', '');
+        //     this.cleanedNumber = '(' + areaCode + ')-' + prefix + '-' + number;
+        //     return;
+        // }
+        //
+        // this.cleanedNumber = '(' + areaCode + ')-' + prefix + '-' + number;
     }
 }
